482f84397b8c05610522242fca0bfadbe08098b7
Cody Jarrett <35110401+codymjarrett@users.noreply.github.com>
2022-03-14 11:20:00 -0500

    make that a string

--- reverseAString/reverse.js
+++ reverseAString/reverse.js
@@ -1,5 +1,5 @@
 `/* 
-    Write a JavaScript function that reverse a number
-    Example x = 32243;
-    Expected Output : 34223
+    Write a JavaScript function called reverseAString that reverses a string
+    Example x = 'Hello World'
+    Expected Output : 'dlroW olleH'
 */
